69037080b7209ed3de91e078c99919b3f35c8e96
Butchman2000 <Bdander2000@msn.com>
2025-05-22 10:37:11 -0400

    Update and rename YOU MUST WAIT_NOT YET.py to one_brick_on_another
    create the necessary

--- Code/YOU MUST WAIT_NOT YET.py
+++
@@ -1,7 +0,0 @@
-# you looked here, didn't you, hahaha
-
-# this is not the time for coding yet, my team will take care of that later
-
-# just talk to me about what you are thinking, in the other folders, as you are a creative person
-
-# we already know that you know how to code, and that you can do it from start to end
